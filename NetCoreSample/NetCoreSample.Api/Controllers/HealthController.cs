541069230d7aee2dce82a6d60bba81aa89a5ceb2
bekoturko <bekirturk@activebuilder.com>
2020-12-11 02:46:49 +0300

    #1 + Varsayılan controller ve model silindi. + HealthCheck için ++ HealthController eklendi.

---
+++ NetCoreSample/NetCoreSample.Api/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreSample.Api.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        #region ctor
+
+        public HealthController()
+        {
+        }
+
+        #endregion
+
+        [HttpGet, Route("health")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
