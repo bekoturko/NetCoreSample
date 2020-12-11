9c6c814037b48ac820bc05965fa6921071bd66f5
bekoturko <bekirturk@activebuilder.com>
2020-12-11 19:04:50 +0300

    #1 - ASP .Net Core Projesinin oluşturulması + Birim testleri için kütüphaneler eklendi. ++ FluentAssertions ++ Moq + Helper sınıfları oluşturuldu. ++ BaseTextFixture ++ TestFixture ++ LooseBaseTextFixture ++ StrictMock + HealthController index metodunun birim testi eklendi.

---
+++ NetCoreSample/NetCoreSample.NUnitTests/ControllerTests/HealthControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreSample.Api.Controllers;
+using NetCoreSample.NUnitTests.Helpers.Fixtures;
+using NUnit.Framework;
+
+namespace NetCoreSample.NUnitTests.ControllerTests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class HealthControllerTests : BaseTestFixture
+    {
+        #region members & setup
+
+        HealthController controller;
+
+        [SetUp]
+        public void Initialize()
+        {
+            controller = new HealthController();
+        }
+
+        #endregion
+
+        #region verify mocks
+
+        protected override void VerifyMocks()
+        {
+            //TODO:
+        }
+
+        #endregion
+
+        [Test]
+        public void Index_NoCondition_ReturnNegotiatedContentResult()
+        {
+            // Arrange
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            result.Should().BeOfType<OkResult>();
+        }
+    }
+}
---
+++ NetCoreSample/NetCoreSample.NUnitTests/Helpers/Fixtures/LooseBaseTestFixture.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreSample.NUnitTests.Helpers.Fixtures
+{
+    /// <summary>
+    /// Strict Mock kullamının gerekmediği TestFixture sınıfları için temel sınıf.
+    /// </summary>
+    public class LooseBaseTestFixture : TestFixture
+    {
+    }
+}
